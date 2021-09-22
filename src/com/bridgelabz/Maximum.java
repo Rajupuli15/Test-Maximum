2c9774613f624ea6743e6e48511a9686bfcd7af1
Rajupuli15 <Rajupuli15@gmail.com>
2021-09-22 03:20:40 -0400

    Added:Maximum_Number

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -2,7 +2,18 @@ package com.bridgelabz;
 
 public class Maximum {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Generic");
+    public static Integer maxInteger(Integer x, Integer y, Integer z) {
+        Integer max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
+
+        if (z.compareTo(max) > 0)
+            max = z;
+            return max;
+        }
+
+        public static void main(String[] args) {
+            System.out.println("Welcome To Generics \n");
+            System.out.println("The maximum value between the three integer is : " + maxInteger(3, 8, 5));
+        }
     }
-}
